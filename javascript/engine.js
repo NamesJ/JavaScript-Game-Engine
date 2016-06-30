f84cc7a987ed165f84f181712e3e6e99a1ef8d98
J Sanders <sanders.j4@gmail.com>
2016-06-30 14:15:31 -0400

    Fixed everything.. I mean everything.
    Refactoring galore, generalizations, and use of modern Javascript
    classes.

--- javascript/engine.js
+++ javascript/engine.js
@@ -1,211 +1,445 @@
-//For debugging purposes FPS is set to default 30
-//Your script must call Game.start();
+//Your script must call game.start();
 //Every GameObject needs to have an update() and draw() method
+// Use requestAnimationFrame to call game.update() and game.draw();
+//
 
-window.onload = function(){
-	init();
-}
-
-var Object = function(x, y){
-	Point2.apply(arguments);
-}
-
-var GameObject = function(){
-	this.rigidbody = null;
-	this.update = function(){}
-	this.draw = function(){}
-}
-
-var Rectangle = function(p, d, c){
-	this.position = p;
-	this.dimensions = d;
-	this.color = c;
-	
-	this.draw = function(){
-		Game.map.fillStyle = this.color;
-		Game.map.fillRect(
-			this.position.x,
-			this.position.y,
-			this.dimensions.x,
-			this.dimensions.y
-		);
-	}
-}
-
-var Physics = function(){
-	Rigidbodies = [];
-	
-	addRigidbody = function( rigidbody ){
-		Rigidbodies.push( rigidbody );
+class Game {
+	constructor( ctx, canvasSize, dimensions, fps, physics ){
+		//canvasSize = Vector( x, y );
+		this.ctx = ctx;
+		this.canvasSize = canvasSize;
+		this.dimensions = dimensions;
+		this.fps = fps;
+		this.physics = physics;
+		this.gameObjects = [];
+		this.startTime;
+		this.framesRun = 0;
+		this.showFPS = false;
+		this.defaultParam = new Array( dimensions ).join("0").split("").map(parseFloat);
 	}
 	
-	run = function(){
-		for ( rigidbody in Rigidbodies ){
-			rigidbody.run();
+	addGameObject( gameObject ){
+		this.gameObjects.push( gameObject );
+	}
+	
+	start(){
+		//Initialization
+		var date = new Date();
+		this.startTime = date.getTime();
+	}
+	
+	blankCanvas(){
+		this.ctx.clearRect( 0, 0, this.canvasSize.c[0], this.canvasSize.c[1] );
+	}
+	
+	update(){
+		// Update rigidBody physics for all rigidBody objects using Physics
+		for( var i=0; i<this.gameObjects.length; i++ ){
+			var gameObject = this.gameObjects[i];
+			
+			if( rigidBody != "Undefined" ){
+				var rigidBody = gameObject.rigidBody;
+				var position = gameObject.position;
+				rigidBody.position = position;
+				rigidBody = this.physics.applyTo( rigidBody, this.fps );
+				gameObject.rigidBody = rigidBody;
+				gameObject.rigidBody.update();
+			}
+			
+			this.gameObjects[i] = gameObject;
+			
+			this.gameObjects[i].update();
+		}
+		
+		//Use Physics to check for collisions between gameObjects
+		this.gameObjects = this.physics.checkCollisions( this.gameObjects );
+		
+		//For showing the FPS in case of potential lag issues
+		if( this.showFPS ){
+			var date = new Date();
+			var now = date.getTime();
+			var time = (now - this.startTime) / 1000;
+			this.framesRun++;
+			var estimateFPS = this.framesRun / time;
+			console.log( "FPS: " + estimateFPS );
+		}
+	}
+	
+	draw(){
+		this.blankCanvas();
+		for( var i=0; i<this.gameObjects.length; i++ ){
+			this.gameObjects[i].draw();
 		}
 	}
 }
 
-var RigidBody = function(m,p){
-	this.mass = m;
-	this.location = p;
-	this.velocity = new Vector2( 0, 0 );
-	this.acceleration = new Vector2( 0, 0 );
-	this.forces = {
-		constant : [],
-		impulse : []
-	};
-	this.freefall = true;
-	this.usesGravity = true;
-	
-	this.setGravity = function( set ){
-		this.usesGravity = set;
+class GameObject {
+	constructor( name, position ){
+		this.name = name;
+		this.position = position;
+		this.rigidBody = "Undefined";
 	}
 	
-	this.addConstantForce = function( force ){
-		this.forces.constant.push( force );
+	setRigidBody( rigidBody ){
+		this.rigidBody = rigidBody;
+		this.rigidBody.position = this.position;
 	}
 	
-	this.addImpulseForce = function(force){
-		this.forces.impulse.push(force);
-	}
+	//If GameObject has a rigidBody, then
+	// the position of the rigidBody becomes
+	// the position of the GameObject
+	update(){}
 	
-	
-	this.update = function(){
-		this.run();
-	}
-	
-	this.run = function(){
-		this.applyGravity();
-		this.applyForces();
-		this.applyAcceleration();
-		this.changePosition();
-		this.checkForGround();
-	}
+	//This method should be overridden by user
+	draw(){}
+}
 
-	this.checkForGround = function(){
-		//Check to see if any collisions have occurred
+class Physics {
+	//Array of rigidbodies
+	constructor( gravity="Undefined" ){
+		this.gravity = gravity;
 	}
 	
-	this.applyGravity = function(){
-		if ( this.usesGravity == true ){
-			this.acceleration = new Vector2(
-				this.acceleration.i,
-				this.acceleration.j + ( Game.levels[ Game.currentLevel ].gravity / this.mass )
-			);
+	//Takes a rigidBody object
+	// applies physics transformations to that object
+	// then returns the altered object
+	applyTo( rigidBody, fps ){
+		//Apply physics here to gb's rigibBody
+		if( this.gravity != "Undefined" ){
+			var deltaGravity = this.gravity.divide(fps);
+			rigidBody.velocity = rigidBody.velocity.add( deltaGravity );
 		}
+		if( rigidBody.velocity != "Undefined" ){
+			var deltaVelocity = rigidBody.velocity.divide(fps);
+			rigidBody.position = rigidBody.position.add( deltaVelocity ) ;
+		}
+		return rigidBody;
 	}
 	
-	//Need to remove forces and or accelerations after they've been added.
-	this.applyForces = function(){
-		for ( force in this.forces.constant ){
-			this.acceleration = new Vector2(
-				this.acceleration.i + ( this.force.i / this.mass ),
-				this.acceleration.j + ( this.force.j / this.mass )
-			);
-		}
-		if(this.forces.impulse != null){
-			for( force in this.forces.impulse){
-				this.acceleration = new Vector2(
-					this.acceleration.i + ( this.force.i / this.mass ),
-					this.acceleration.j + ( this.force.j / this.mass )
-				);
+	checkCollisions( gameObjects ){
+		var k = 0;
+		var size = gameObjects.length;
+		
+		while( k < size ){
+			if( gameObjects[k].rigidBody != "Undefined" ){
+				for( var i=k+1; i<size; i++ ){
+					if( gameObjects[k].rigidBody.collider.isColliding( gameObjects[i].rigidbody.collider ) ){
+						gameObjects[k].rigidBody.collisions[ gameObjects.rigidBody ];
+						gameObjects[i].rigidBody.collisions[ gameObjects.rigidBody ];
+					}
+				}
+				k++;
 			}
 		}
-		this.forces.impulse = [];
-	}
-
-	this.applyAcceleration = function(){
-		this.velocity = new Vector2(
-			this.velocity.i + this.acceleration.i,
-			this.velocity.j + this.acceleration.j
-		);
-	}
-	
-	this.changePosition = function(){
-		this.location = new Point2( 
-			this.location.i + this.velocity.i,
-			this.location.j + this.velocity.j
-		);
+		
+		return gameObjects;
 	}
 }
 
-//this is useless
-var Mass = function(m){
-	mass = m;
-}
-
-var Point2 = function(newX, newY){
-	this.x = newX;
-	this.y = newY;
-}
-
-var Vector2 = function(newI, newJ){
-	this.i = newI;
-	this.j = newJ;
-	
-	getMagnitude = function(){
-		// sqrt( i^2 + j^2 );
-		return Math.sqrt( Math.pow(i, 2) + Math.pow(j, 2) );
+//User needs to provide dimensionality
+class RigidBody {
+	constructor( position ){
+		this.mass = new Mass();
+		this.position = position;
+		this.velocity = "Undefined";
+		this.forces = [];
+		this.colliders = [];
+		this.useGravity = false;
 	}
 	
-	getDirection = function(){
-		//returns a new Point object representing unit vector
-		console.log( position );
-		return new Vector2( 
-			( position.get_x() / getMagnitude() ) ,
-			( position.get_y() / getMagnitude() ) 
-		);
+	update(){
+		//if collisions, then calculate deflection
+		// then pop the collision
+		if( this.colliders.length > 0 ){
+			//there are some collisions
+			
+		}
+		return this.position;
+	}
+	
+	addCollider(){
+		var numArguments = arguments.length;
+		for( var i=0; i<numArguments; i++ ){
+			colliders.push( argument[i] );
+		}
 	}
 }
 
-var Force2 = function(i, j , isConstant){
-	Vector2.apply(arguments[0], arguments[1]);
-		//Ensures duration in seconds
-		constant = isConstant;
-}
-
-var Impulse2 = function( i, j ){
-	Force2.apply(arguments, 1/game.FPS);
-}
-
-var Gravity = function( i, j, duration ){
-	Force2.apply(arguments);
-}
-
-var Game = {
-	ready : false,
-	FPS : 30,
-	levels : [],
-	dimensions : new Point2( 800, 600 ),
-	paused : false,
-	currentLevel : 0,
-	
-	//Start Employment of levels Here
-	startLevel : function(){
-		this.levels[ this.currentLevel ].start();
-	},
-	
-	start : function(){
-		this.startLevel();
-	},
-	
-	loadNextLevel : function(){
-		this.currentLevel++;
-	},
-	
-	pause : function(){
-		this.paused = !paused;
-	},
-	
-	addLevel : function( level ){
-		this.levels.push( level );
+/*
+	Collider types
+	----------------
+	Box			- Simple box with width and height
+	Circle      - Simple circle with a radius
+*/
+class Collider {
+	constructor( type="Undefined" ){
+		this.position = new Vector( Game.defaultParam );
+		this.type = type;
+		//Colorize the collider for debugging
+		this.collisions = [];
+		this.debug = false;
 	}
-};
+}
 
-//Initalization
-function init(){
-	var canvas = document.getElementById('map');
-	Game.map = canvas.getContext('2d');
-	Game.ready = true;
+Collider.prototype = {
+	isColliding( otherCollider ){
+		if( otherCollider.position.x == this.position.x &&
+			otherCollider.position.y == this.position.y &&
+			otherCollider.position.z == this.position.z )
+		{ 
+			return true;
+		}
+		
+		return false;
+	}
+}
+
+class SphereCollider extends Collider {
+	//Collider.apply( offset, "Sphere" );
+	constructor( offset, radius ){
+		super( "SphereCollider" );
+		this.radius = radius;
+		this.type = "SphereCollider";
+	}
+	
+	isColliding( otherCollider ){
+		if( otherCollider.type == "SphereCollider"){
+			var position = this.position.add( objectPosition );
+			var distance = position.distance( otherCollider.position );
+			var sumRadii = this.radius + otherCollider.radius;
+			
+			if( distance < sumRadii ){
+				return true;
+			}
+			
+			return false;
+		}
+	}
+}
+
+class BoxCollider extends Collider {
+	constructor( position, shape ){
+		Collider.apply( position );
+		this.shape = shape;
+	}
+}
+
+class Mass{
+	constructor(m){
+		this.mass = m;
+	}
+}
+
+class Vector {
+	// Vector can be of any number of dimensions
+	constructor(){
+		this.c = arguments;
+		this.size = arguments.length;
+	}
+	
+	getMagnitude()
+	{
+		var magnitude = 0.0;
+		
+		// Add each squared component
+		for( var i=0; i<this.c.length; i++ ){
+			magnitude = magnitude + Math.pow( parseFloat(this.c[i]), 2.0 );
+		}
+		
+		// Take sqare root of sum
+		magnitude = Math.sqrt( magnitude );
+		
+		return magnitude;
+	}
+	
+	getDirection(){
+		
+		var components = [];
+		
+		// Arguments format:  { "0": 1, "1": 2, "2": 3 }
+		for( var i=0; i<this.c.length; i++ ){
+			var parameter = {};
+			components.push( this.c[i] / this.getMagnitude() );
+		}
+		
+		var dirVector = new Vector;
+		dirVector.c = components;
+		
+		return dirVector;
+	}
+	
+	distance( otherVector ){
+		//return distance between to vectors centered at (0,0,...,0)
+		var distance = 0;
+		var tmp = new Vector( 0,0 );
+		var numArguments = arguments.length;
+		for( var i=0; i<numArguments; i++ ){
+			tmp.c[i] = otherVector.c[i] - this.c[i];
+		}
+		distance = tmp.getMagnitude();
+		return distance;
+	}
+	
+	// Vector addition
+	add( addend ){
+		var sum = new Vector();
+		sum.c = this.c;
+		
+		var maxSize = this.size;
+		if( addend.size > maxSize ){
+			maxSize = addend.size;
+			sum.c = addend.c;
+			
+			for( var i=0; i<maxSize; i++ ){
+				if( typeof this.c[i] == "undefined" ){
+					// Nothing to add
+				}else{
+					sum.c[i] += this.c[i];
+				}
+			}
+		}else{
+			for( var i=0; i<maxSize; i++ ){
+				if( typeof addend.c[i] == "undefined" ){
+					// Nothing to add
+				}else{
+					sum.c[i] += addend.c[i];
+				}
+			}
+		}
+		return sum;		
+	}
+	
+	//Vector subtraction
+	subtract( subtrahend ){
+		var sum = new Vector();
+		sum.c = this.c;
+		
+		var maxSize = this.size;
+		if( subtrahend.size > maxSize ){
+			maxSize = subtrahend.size;
+			sum.c = subtrahend.c;
+			
+			for( var i=0; i<maxSize; i++ ){
+				if( typeof this.c[i] == "undefined" ){
+					// Nothing to add
+				}else{
+					sum.c[i] += this.c[i];
+				}
+			}
+		}else{
+			for( var i=0; i<maxSize; i++ ){
+				if( typeof subtrahend.c[i] == "undefined" ){
+					// Nothing to add
+				}else{
+					sum.c[i] += subtrahend.c[i];
+				}
+			}
+		}
+		
+		return sum;	
+	}
+	
+	// Scalar multiplication
+	multiply( multiplier ){
+		var tmp = new Vector();
+		tmp.c = [];
+		
+		for( var i=0; i<this.size; i++ ){
+			tmp.c.push( this.c[i] * multiplier );
+		}
+		
+		return tmp;
+	}
+	
+	//Scalar division
+	divide( divisor ){
+		var tmp = new Vector();
+		tmp.c = [];
+		
+		for( var i=0; i<this.size; i++ ){
+			tmp.c.push( this.c[i] * divisor );
+		}
+		
+		return tmp;
+	}
+	
+	//Vector dot product
+	dot(){
+		var numArguments = arguments.length;
+		var minNumComponents = Infinity;
+		
+		for( var i=0; i<numArguments; i++ ){
+			if( arguments[i] instanceof Vector ){
+				if( arguments[i].c.length < minNumComponents ){
+					minNumComponents = arguments[i].c.length;
+				}
+			}
+		}
+		
+		var dotProduct = 0;
+		
+		for( var i=0; i<minNumComponents; i++ ){
+			var product = 1;
+			
+			for( var j=0; j<numArguments; j++ ){
+				if( typeof arguments[j].c[i] == "undefined" ){
+					//product would be 0
+					//nothing to add
+				}else{
+					product = this.c[i] * arguments[j].c[i];
+				}
+			}
+			
+			dotProduct += product;
+		}
+	
+		return dotProduct;
+	}
+	
+	//Vector cross product
+	cross(){
+		//This will be resolved later when the 
+		// computation methods which I understand are more rigorous
+		var crossProduct = "This has yet to be implemented. Sorry :(";
+		return crossProduct;
+	}
+}
+
+class Force extends Vector {
+	//Force is constant if arguments[numArguments] == Infinity
+	//Final argument i.e( arguments[numArguments] ) 
+	// should be set to milliseconds to apply force
+	constructor( force, time, fps ){
+		var framesToApply = time * fps;
+		//Make the force components such that they
+		// are scaled to the force per frame
+		for ( var i=0; i<force.size; i++){
+			force.c[ i ] = force.c[ i ] / framesToApply;
+		}
+		super( force );
+		this.framesRemaining = framesToApply;
+	}
+	
+	apply(){
+		this.framesRemaining--;
+	}
+	
+	isDone(){
+		var done = ( this.framesRemaining <= 0 );
+		return done;
+	}
+}
+class Impulse extends Force {
+	constructor(force, fps){
+		this.framesRemaining = 1;
+		super( force, framesToApply );
+	}
+}
+
+class Gravity extends Force {
+	//Gravity is a constant force
+	// therefore its time is Infinity
+	constructor( gForce, fps ){
+		super( gForce, Infinity );
+	}
 }
--- javascript/game.js
+++ javascript/game.js
@@ -1,265 +1,87 @@
-var Player;
-var Level1;
-var key = {};
-var levels = [];
-/**
-var _Player = {
-	groundState : 'freefall',
-	up : new Vector2( 0, 10, 1/Game.FPS ),
-	left : new Vector2( -5, 0, 0.1 ),
-	right : new Vector2( 5, 0, 0.1 ),
-	down : new Vector2( 0, 0, 1/Game.FPS ),
+var g = window.onload = function(){
+	var canvas = document.getElementById("GameMap");
+	var ctx = canvas.getContext("2d");
+	//Get instance of Game
+	var dimensions = 2;
+	var fps = 30;
+	//Create Physics object
+	var physics = new Physics();
+	//Set size of canvas
+	var width = 800;
+	var height= 500;
+	var canvasSize = new Vector( width, height );
+	var game = new Game( ctx, canvasSize, dimensions, fps, physics );
+	//Assign gravity object
+	//game.physics.gravity = new Gravity( 0, -1, 0);
 	
-	addComponent : function( object ){
-		if ( object.typeof( RigidBody ) ){
-			rigidbody = object;
-		} else if ( object.typeof ( Force2 ) ){
-			rigidbody.addForce( object );
-		}
-	},
+	var key = [];
+
+	//Create player GameObject
+	var position = new Vector(100,100);
+	var player = new GameObject( "Player", position );
+	//Define player rigidBody attribute
+	var rigidBody = new RigidBody();
+	player.setRigidBody( rigidBody );
+	//Define player colliders
+	var coll = new SphereCollider( player.position, 1 );
+	player.rigidBody.colliders.push( coll );
 	
-	jump : function(){
-		rigidbody.addForce( new Vector2( 0, 20 ) )
-	},
-	
-	move : function( direction ){
-		rigidbody.addForce( direction );
-	},
-	
-	draw : function(){
-		
+	//Define draw method
+	player.draw = function(){
+		//console.log( this.position.c );
+		//console.log( this.rigidBody.velocity.c );
+		ctx.fillStyle = "#333333";
+		ctx.beginPath();
+		ctx.arc( this.position.c[0], this.position.c[1], 20, 0, 2*Math.PI );
+		ctx.stroke();
 	}
+
+	//Add player to game
+	player.rigidBody.velocity = new Vector( 0.1, 0 );
+	game.addGameObject( player   );
 	
+	//Create platform object
+	var platform = new GameObject("Platform")
+	platform.position = new Vector( 200, 100 );
+	platform.rigidBody = new RigidBody();
+	
+	//Define draw method
+	platform.draw = function(){
+		ctx.fillStyle = "#333333";
+		ctx.beginPath();
+		ctx.arc( this.position.c[0], this.position.c[1], 20, 0, 2*Math.PI );
+		ctx.stroke();
+	}
+
+	//Define colliders for platform
+	coll = new SphereCollider( platform.position, 2 );
+	platform.rigidBody.colliders.push( coll );
+	
+	//Add platform to game
+	game.addGameObject( platform );
+	
+	var loop = function(){
+		setTimeout( function(){
+			window.requestAnimationFrame( loop );
+			game.update()
+			game.draw();
+		}, 1000 / Game.fps);
+	}
+
+	//Input Event Handling System
+	document.addEventListener("keydown", function(evt) {
+		if (evt.keyCode != 80) {
+			key[evt.keyCode] = true;
+		}
+		else {
+			pause();
+		}
+	});
+	 
+	document.addEventListener("keyup", function(evt) {
+		key[evt.keyCode] = false;
+	});
+	
+	game.start();
+	loop();
 }
-**/
-
-//Level Design
-
-/*Levels must have 
-	start(), start the level
-	update(), update the physics
-	draw(),  draw everything to the canvas
-	getAll(), return a 'copy' of the level for Game to use
-functions
-and
-	GameObjects[], to hold all objects within the GameObjects
-	gravity vector, [optional-ish]
-	player, to play the Game obviously
-
-*/
-
-function setup(){
-	Player = {};
-	Player.body = [];
-	Player.position = new Point2(Game.dimensions.x / 2, Game.dimensions.y / 2);
-	Player.mass = 1;
-	Player.rigidbody = new RigidBody( Player.mass , Player.position );
-	Player.up = new Vector2( 0, 10, 1 / Game.FPS );
-	Player.left = new Vector2( -5, 0, 0.1 );
-	Player.right = new Vector2( 5, 0, 0.1 );
-	Player.down = new Vector2( 0, 0, 1 / Game.FPS );
-	Player.resetPosition = function(){
-		this.position = new Point2(
-			Game.dimensions.x / 2,
-			Game.dimensions.y / 2
-		);
-	};
-	Player.init = function(){
-		position = new Point2(Player.position.x, Player.position.y);
-		head = new Rectangle(
-			new Point2(
-				position.x - 3,
-				position.y + 10
-			),
-			new Point2(
-				20,
-				20
-			),
-			"#FFCCBB"
-		);
-		torso = new Rectangle(
-			new Point2(
-				position.x - 10,
-				position.y + 30
-			),
-			new Point2(
-				35,
-				60
-			),
-			"#FFBB66"
-		),
-		pantBase = new Rectangle(
-			new Point2(
-				position.x - 10,
-				position.y + 90
-			),
-			new Point2(
-				35,
-				10
-			),
-			"#0099BB"
-		);
-		pantLegs = new Rectangle(
-			new Point2(
-				position.x - 3,
-				position.y + 100
-			),
-			new Point2(
-				20,
-				50
-			),
-			"#0099BB"
-		);
-		shoeL = new Rectangle(
-			new Point2(
-				position.x - 4,
-				position.y + 150
-			),
-			new Point2(
-				15,
-				7
-			),
-			"#FFBB66"
-		);
-		shoeR = new Rectangle(
-			new Point2(
-				position.x + 10,
-				position.y + 150
-			),
-			new Point2(
-				15,
-				7
-			),
-			"#FFBB66"
-		);
-		Player.body.push( head     );
-		Player.body.push( torso    );
-		Player.body.push( pantBase );
-		Player.body.push( pantLegs );
-		Player.body.push( shoeL    );
-		Player.body.push( shoeR    );
-	};
-	Player.jump = function(){
-		Player.rigidbody.addImpulseForce( Player.up )
-	};
-	Player.move = function( direction ){
-		Player.rigidbody.addImpulseForce( direction );
-	};
-	Player.userInput = function(){
-		//Handle the users input here
-		if (Game.paused == false) {
-			if (key[87]){
-				//W Key
-				//If on platform
-				if (Player.groundState == 'grounded') {
-					Player.jump();
-				}
-			}
-			
-			if (key[65]){
-				//A Key
-				Player.move( Player.left );
-			}
-			
-			if (key[83]){ 
-				//S Key
-				Player.move( Player.down );
-			}
-			
-			if (key[68]){
-				//D Key
-				Player.move( Player.right );
-			}
-		/*
-			if (key[32]){
-				//Spacebar
-				if (jetPack.has == true){
-					rigidbody.addForce( new Vector2( 0, jetpack.force ) );
-				}
-			}
-		*/
-		}
-	};
-	Player.update = function(){
-		Player.userInput();
-		Player.rigidbody.update();
-	};
-	Player.draw = function(){
-		for( var x = 0; x < Player.body.length; x++ ){
-			Player.body[ x ].draw();
-		}
-	};
-
-	Level1 = {};
-	Level1.title = "null";
-	Level1.GameObjects = [];
-	Level1.gravity = -9.81;
-	Level1.setTitle = function(t){
-		title = t;
-	};
-	Level1.setupEnvironment = function(){
-		return true;
-	};
-	Level1.setupPickups = function(){
-		return true;
-	};
-	Level1.setupPlayer = function(){
-		Player.resetPosition();
-	};
-	Level1.addGameObject = function( object ){
-		Level.GameObjects.push( object );
-	};
-	Level1.draw = function(){
-		if ( Game.paused == false ){
-			Player.draw();
-			for( object in Level1.GameObjects ){
-				object.draw();
-			}
-			requestAnimationFrame( Level1.update );
-		} else {
-			//requestAnimationFrame( pauseScreen );
-		}
-	};
-	Level1.update = function(){
-		Player.update();
-		for( object in Level1.GameObjects ){
-			object.update();
-		}
-		Level1.draw();
-	};
-	Level1.start = function(){
-		//setup the level here
-		Level1.setupEnvironment();
-		Level1.setupPickups();
-		Level1.setupPlayer();
-		Level1.update();
-	};
-}
-
-//Input Event Handling System
-document.addEventListener("keydown", function(evt) {
-	if (evt.keyCode != 80) {
-		key[evt.keyCode] = true;
-	}
-	else {
-		pause();
-	}
-});
- 
-document.addEventListener("keyup", function(evt) {
-    key[evt.keyCode] = false;
-});
-
-function start(){
-	if(Game.ready == true){
-		setup();
-		Player.init();
-		Level1.setTitle("Level 1");
-		Game.addLevel( Level1 );
-		Game.start();
-	}else{
-		requestAnimationFrame(start);
-	}
-}
-
-start();
